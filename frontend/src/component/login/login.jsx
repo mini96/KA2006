f1ca8194b78341652d01bedc7d873b9e5be58e6d
Menelik Gedion <menilik98@gmail.com>
2020-04-02 21:17:17 +1100

    Draft for the setup

--- frontend/src/component/login/login.jsx
+++ frontend/src/component/login/login.jsx
@@ -2,36 +2,40 @@ import React from "react"
 import loginImg from"../../login.svg";
 
 
-export class login extends React.Component{
+export class Login extends React.Component{
 
     constructor(props){
         super(props);
     }
 
     render(){
-        return(
-        <div className="base-container">
-        <div className="header"></div>
-        <div className="content"></div>
-        <div className="image">
-            <img src={loginImg} />
-            <div className="form">
-             <div className="form-group">
-                 <label htmlFor="Username"></label>
-                 <input type="text" name="username" placeholder="username"/>
-             </div>
-             <div className="form-group">
-                 <label htmlFor="Password"></label>
-                 <input type="text" name="password" placeholder="password"/>
-             </div>
+        return (
+          <div className="base-container">
+            <div className="header"></div>
+            <div className="content"></div>
+            <div className="image">
+              <img src={loginImg} />
+              <div className="form">
+                <div className="form-group">
+                  <label htmlFor="Username">Username</label>
+                  <input type="text" name="username" placeholder="username" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="Password">Password</label>
+                  <input type="text" name="password" placeholder="password" />
+                </div>
+              </div>
             </div>
-        </div>
-        <div className="footer">
-            <button type="button" className="btn">
+            <div className="footer">
+              <button type="button" className="btn">
                 Submit
-            </button>
-        </div>
-        </div>)
+              </button>
+              <button type="button" className="btn">
+                Guest login
+              </button>
+            </div>
+          </div>
+        );
     };
 
 }
--- frontend/src/component/login/register.jsx
+++ frontend/src/component/login/register.jsx
@@ -1,37 +1,42 @@
-import React from "react"
-import loginImg from"../../login.svg";
+import React from "react";
+import loginImg from "../../login.svg";
 
+export class Register extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-export class login extends React.Component{
-
-    constructor(props){
-        super(props);
-    }
-
-    render(){
-        return(
-        <div className="base-container">
+  render() {
+    return (
+      <div className="base-container">
         <div className="header"></div>
         <div className="content"></div>
         <div className="image">
-            <img src={loginImg} />
-            <div className="form">
-             <div className="form-group">
-                 <label htmlFor="Username"></label>
-                 <input type="text" name="username" placeholder="username"/>
-             </div>
-             <div className="form-group">
-                 <label htmlFor="Password"></label>
-                 <input type="text" name="password" placeholder="password"/>
-             </div>
+          <img src={loginImg} />
+          <div className="form">
+            <div className="form-group">
+              <label htmlFor="Username">Username</label>
+              <input type="text" name="username" placeholder="username" />
             </div>
+            <div className="form-group">
+              <label htmlFor="Email">Email</label>
+              <input type="text" name="email" placeholder="email" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="Password">Password</label>
+              <input type="text" name="password" placeholder="password" />
+            </div>
+          </div>
         </div>
         <div className="footer">
-            <button type="button" className="btn">
-                Submit
-            </button>
+          <button type="button" className="btn">
+            Register
+          </button>
+          <button type="button" className="btn">
+            Guest login
+          </button>
         </div>
-        </div>)
-    };
-
+      </div>
+    );
+  }
 }
