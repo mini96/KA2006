8bf6b637a59289a5bbe26e1a51a326ff479f45fb
Menelik Gedion <menilik98@gmail.com>
2020-04-06 21:16:10 +1000

    Final iteration of Login page

--- frontend/src/component/login_components/login.jsx
+++ frontend/src/component/login_components/login.jsx
@@ -1,14 +1,69 @@
 import React from "react";
 import loginImg from "../../logo2.PNG";
 
+const initialState = {
+  
+  email: "",
+  password: "",
+  
+  emailError: "",
+  passwordError: ""
+};
+
 export class Login extends React.Component {
   constructor(props) {
     super(props);
   }
+  state = initialState;
+
+
+  handleChange = event => {
+    const isCheckbox = event.target.type === "checkbox";
+    this.setState({
+      [event.target.name]: isCheckbox
+        ? event.target.checked
+        : event.target.value
+    });
+  };
+
+  validate = () => {
+    
+    let emailError = "";
+    let passwordError = "";
+
+    if (!this.state.email) {
+      emailError = "Email cannot be blank";
+    }
+    else if (!/^[a-zA-Z0-9]+@+[a-zA-Z0-9]+.+[A-z]/.test(this.state.email)){
+      emailError = "Email is invalid";
+    }
+
+    if (!this.state.password) {
+      passwordError = "Password cannot be blank";
+    }
+
+    if (emailError || passwordError) {
+      this.setState({ emailError, passwordError });
+      return false;
+    }
+
+    return true;
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const isValid = this.validate();
+    if (isValid) {
+      console.log(this.state);
+      // clear form
+      this.setState(initialState);
+    }
+  };
+
 
   render() {
     return (
-      <div className="base-container" ref={this.props.containerRef}>
+      <form className="base-container" ref={this.props.containerRef} onSubmit={this.handleSubmit}>
         <div className="header">Red-Shift Estimator</div>
         <div className="content">
           <div className="image">
@@ -17,23 +72,39 @@ export class Login extends React.Component {
           <div className="form">
             <div className="form-group">
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" placeholder="email" />
+              <input 
+              type="text" 
+              name="email" 
+              placeholder="email"
+              value={this.state.email}
+              onChange={this.handleChange} />
+            </div>
+            <div className="error">
+              {this.state.emailError}
             </div>
             <div className="form-group">
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" placeholder="password" />
+              <input 
+              type="password" 
+              name="password" 
+              placeholder="password" 
+              value={this.state.password}
+              onChange={this.handleChange}/>
+            </div>
+            <div className = "error" >
+              {this.state.passwordError}
             </div>
           </div>
         </div>
         <div className="footer">
-          <button type="button" className="btn">
+          <button type="submit" className="btn">
             Login
           </button>
           <button type="button" className="btn">
             Guest Login
           </button>
         </div>
-      </div>
+      </form>
     );
   }
 }
