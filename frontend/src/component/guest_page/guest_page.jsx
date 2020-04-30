5631bebd91d6addeded63587c9b6367d6f0a2763
Menelik Gedion <menilik98@gmail.com>
2020-04-30 22:41:32 +1000

    draft input fields

--- frontend/src/component/guest_page/guest_page.jsx
+++ frontend/src/component/guest_page/guest_page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './guest_page.scss';
 import Input_fields from '../input_field/input_fields'
-// import Button from "react-bootstrap/Button";
+ import Button from "react-bootstrap/Button";
 // import InputGroup from 'react-bootstrap/InputGroup'
 
 // import FormControl from "react-bootstrap/FormControl";
@@ -13,12 +13,17 @@ import Input_fields from '../input_field/input_fields'
 
 
 class guest_page extends React.Component {
-  
+  state =  {
+    visable : false
+  };
   // constructor(props) {
   //   super(props);
   // }
   
+  fileSelectedHandler = event => {
+    console.log(event.target.files[0]);
 
+  }
     render() {
 
 
@@ -40,21 +45,42 @@ class guest_page extends React.Component {
                  <br />
             between the galaxies you want to get.
           </p>
-              </div>
-          
+              
             
-          
-          
-            <div className="guest_container">
-             
-            <Input_fields />
-            </div>   
+                
+       
+
+            <input type="file" onChange={this.fileSelectedHandler} />
+              <button onClick={this.fileUploadHandler} class="btn btn-light">Upload</button>
+           
+            <p>
+              Or if you are just looking to calculate for a single object then 
+              press the button below
+            </p>
+              <button type="button" class="btn btn-light" onClick={() =>{
+              this.setState({ visable : !this.state.visable });
+            }} >Proceed</button>
+</div>
+            {this.state.visable ? <div className="guest_container"> <Input_fields /> </div> : null}
+         
+         
+         
+           {/* 
+           <Input_fields />
+           
+           </div>
+             */}
+      
+            
+           
 
 
             <div className="output_box">
             <p>The output still has yet to come</p>
             </div>
           <div className="footer"></div>
+          
+
           </div>
           
         );
--- frontend/src/component/input_field/input_fields.jsx
+++ frontend/src/component/input_field/input_fields.jsx
@@ -6,33 +6,34 @@ import InputGroup from 'react-bootstrap/InputGroup'
 
 
 const initialState = {
+    selectFile: null,
     id: "",
     optical1: "",
     optical2: "",
     optical3: "",
     optical4: "",
     optical5: "",
-    infared1: "",
-    infared2: "",
-    infared3: "",
-    infared4: "",
-    infared5: "",
-    infared6: "",
+    infrared1: "",
+    infrared2: "",
+    infrared3: "",
+    infrared4: "",
+    infrared5: "",
+    infrared6: "",
     radio: "",
 
 
-
+    optical_e: "",
     optical1_e: "",
     optical2_e: "",
     optical3_e: "",
     optical4_e: "",
     optical5_e: "",
-    infared1_e: "",
-    infared2_e: "",
-    infared3_e: "",
-    infared4_e: "",
-    infared5_e: "",
-    infared6_e: "",
+    infrared1_e: "",
+    infrared2_e: "",
+    infrared3_e: "",
+    infrared4_e: "",
+    infrared5_e: "",
+    infrared6_e: "",
     radio_e: "",
 };
 
@@ -43,9 +44,9 @@ class input_field extends React.Component {
     handleSubmit = (event) => {
         event.preventDefault();
         const isValid = this.validate();
-
         if (!isValid) {
             console.log(this.state);
+            this.state.optical_e = "";
             // clear error from form
             //  this.state.optical1_e="";
             //  this.state.optical2_e="";
@@ -77,25 +78,28 @@ class input_field extends React.Component {
 
 
     validate = () => {
-        let optical1_e: "";
-        let optical2_e: "";
-        let optical3_e: "";
-        let optical4_e: "";
-        let optical5_e: "";
-        let infared1_e: "";
-        let infared2_e: "";
-        let infared3_e: "";
-        let infared4_e: "";
-        let infared5_e: "";
-        let infared6_e: "";
-        let radio_e: "";
+        let optical_e = "";
+        let optical1_e = "";
+        let optical2_e =  "";
+        let optical3_e = "";
+        let optical4_e = "";
+        let optical5_e =  "";
+        let infrared_e = "";
+        let infrared1_e =  "";
+        let infrared2_e =  "";
+        let infrared3_e = "";
+        let infrared4_e = "";
+        let infrared5_e = "";
+        let infrared6_e = "";
+        let radio_e = "";
 
 
         if (!this.state.optical1){
           
         }
         else if (!/^[0-9]+([,.][0-9]+)?$/.test(this.state.optical1)) {
-            optical1_e = "optical is not suppose to have words or special characters";
+            //optical1_e = "optical is not suppose to have words or special characters";
+            optical_e = "optical is not suppose to have words or special characters";
             console.log("this shit works");
         }
 
@@ -103,70 +107,80 @@ class input_field extends React.Component {
 
         }
         else if (!/^[0-9]+([,.][0-9]+)?$/.test(this.state.optical2)) {
-            optical2_e = "optical is not suppose to have words or special characters";
+           // optical2_e = "optical is not suppose to have words or special characters";
+            optical_e = "optical is not suppose to have words or special characters";
         }
 
         if (!this.state.optical3) {
 
         }
         else if (!/^[0-9]+([,.][0-9]+)?$/.test(this.state.optical3)) {
-            optical3_e = "optical is not suppose to have words or special characters";
+            //optical3_e = "optical is not suppose to have words or special characters";
+            optical_e = "optical is not suppose to have words or special characters";
         }
 
         if (!this.state.optical4) {
 
         }
         else if (!/^[0-9]+([,.][0-9]+)?$/.test(this.state.optical4)) {
-            optical4_e = "optical is not suppose to have words or special characters";
+            //optical4_e = "optical is not suppose to have words or special characters";
+            optical_e = "optical is not suppose to have words or special characters";
         }
 
         if (!this.state.optical5) {
 
         }
         else if (!/^[0-9]+([,.][0-9]+)?$/.test(this.state.optical5)) {
-            optical5_e = "optical is not suppose to have words or special characters";
+           // optical5_e = "optical is not suppose to have words or special characters";
+            optical_e = "optical is not suppose to have words or special characters";
         }
 
-        if (!this.state.infared1) {
+        if (!this.state.infrared1) {
 
         }
-        else if (!/^[0-9]+([,.][0-9]+)?$/.test(this.state.infared1)) {
-            infared1_e = "infared is not suppose to have words or special characters";
+        else if (!/^[0-9]+([,.][0-9]+)?$/.test(this.state.infrared1)) {
+           // infrared1_e = "infared is not suppose to have words or special characters";
+            infrared_e = "infared is not suppose to have words or special characters";
         }
 
-         if (!this.state.infared2){
+         if (!this.state.infrared2){
         
         }
-        else if (!/^[0-9]+([,.][0-9]+)?$/.test(this.state.infared2)) {
-            infared2_e = "infared is not suppose to have words or special characters";
+        else if (!/^[0-9]+([,.][0-9]+)?$/.test(this.state.infrared2)) {
+           // infrared2_e = "infared is not suppose to have words or special characters";
+             infrared_e = "infared is not suppose to have words or special characters";
         }
 
-         if (!this.state.infared3){
+         if (!this.state.infrared3){
         
         }
-        else if (!/^[0-9]+([,.][0-9]+)?$/.test(this.state.infared3)) {
-            infared3_e = "infared is not suppose to have words or special characters";
+        else if (!/^[0-9]+([,.][0-9]+)?$/.test(this.state.infrared3)) {
+            //infrared3_e = "infared is not suppose to have words or special characters";
+             infrared_e = "infared is not suppose to have words or special characters";
         }
 
-         if (!this.state.infared4){
+         if (!this.state.infrared4){
         
         }
-        else if (!/^[0-9]+([,.][0-9]+)?$/.test(this.state.infared4)) {
-            infared4_e = "infared is not suppose to have words or special characters";
+        else if (!/^[0-9]+([,.][0-9]+)?$/.test(this.state.infrared4)) {
+           // infrared4_e = "infared is not suppose to have words or special characters";
+             infrared_e = "infared is not suppose to have words or special characters";
         }
 
-         if (!this.state.infared5){
+         if (!this.state.infrared5){
         
         }
-        else if (!/^[0-9]+([,.][0-9]+)?$/.test(this.state.infared5)) {
-            infared5_e = "infared is not suppose to have words or special characters";
+        else if (!/^[0-9]+([,.][0-9]+)?$/.test(this.state.infrared5)) {
+            //infrared5_e = "infared is not suppose to have words or special characters";
+             infrared_e = "infared is not suppose to have words or special characters";
         }
 
-         if (!this.state.infared6){
+         if (!this.state.infrared6){
         
         }
-        else if (!/^[0-9]+([,.][0-9]+)?$/.test(this.state.infared6)) {
-            infared6_e = "infared is not suppose to have words or special characters";
+        else if (!/^[0-9]+([,.][0-9]+)?$/.test(this.state.infrared6)) {
+           // infrared6_e = "infared is not suppose to have words or special characters";
+             infrared_e = "infared is not suppose to have words or special characters";
         }
 
          if (!this.state.radio){
@@ -177,45 +191,90 @@ class input_field extends React.Component {
         }
 
 
-
-        if (optical1_e || optical2_e || optical3_e || optical4_e || optical5_e ||
-            infared1_e || infared2_e || infared3_e || infared4_e || infared5_e || infared6_e || radio_e) {
-            this.setState({ optical1_e, optical2_e, optical3_e, optical4_e, optical5_e, infared1_e, infared2_e, infared3_e, infared4_e, infared5_e, infared6_e, radio_e });
+if (optical_e || infrared_e || radio_e) {
+            this.setState({ optical_e, infrared_e, radio_e });
             return false;
         }
+
+
+        // if (optical1_e || optical2_e || optical3_e || optical4_e || optical5_e ||
+        //     infrared1_e || infrared2_e || infrared3_e || infrared4_e || infrared5_e || infrared6_e || radio_e) {
+        //     this.setState({ optical1_e, optical2_e, optical3_e, optical4_e, optical5_e, infrared1_e, infrared2_e, infrared3_e, infrared4_e, infrared5_e, infrared6_e, radio_e });
+        //     return false;
+        // }
 
         return true;
     };
 
+ 
 
     render() {
-
+        
         return (
             <div>
 
                 <form onSubmit={this.handleSubmit}>
 
+                    <div className = "input_fe">
 
-                    <InputGroup size="sm" className="mb-3"
-                        onChange={this.handleChange}
-                    // onChange={this.handleChange}  
-                    >
-                        <InputGroup.Prepend >
-                            <InputGroup.Text id="inputGroup-sizing-sm">ID</InputGroup.Text>
-                        </InputGroup.Prepend>
-                        <FormControl
-                            type="text"
-                            name="id"
-                            aria-label="Small"
-                            aria-describedby="inputGroup-sizing-sm"
-                        /> 
-                    </InputGroup>
+                        <div className="input-group">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text" id="">ID</span>
 
+
+                                <input type="text" class="form-control" placeholder="ID" name="id" onChange={this.handleChange} />
+
+                            </div>
+                            </div>
+                            <div className="error">{this.state.radio_e}</div>
+
+
+                        <div className="input-group">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text" id="">Optical</span>
+                            </div>
+                            <input type="text" class="form-control" placeholder="u" name="optical1" onChange={this.handleChange} />
+                            <input type="text" class="form-control" placeholder="g" name="optical2" onChange={this.handleChange}/>
+                            <input type="text" class="form-control" placeholder="r" name="optical3" onChange={this.handleChange}/>
+                            <input type="text" class="form-control" placeholder="i" name="optical4" onChange={this.handleChange}/>
+                            <input type="text" class="form-control" placeholder="z" name="optical5" onChange={this.handleChange}/>
+                            <div className="error">{this.state.optical_e}</div>
+</div>
+                            <div className="input-group">
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text" id="">Infrared</span>
+                                </div>
+                                <input type="text" class="form-control" placeholder="3.6" name="infrared1" onChange={this.handleChange} />
+                                <input type="text" class="form-control" placeholder="4.5" name="infrared2" onChange={this.handleChange} />
+                                <input type="text" class="form-control" placeholder="5.8" name="infrared3" onChange={this.handleChange} />
+                                <input type="text" class="form-control" placeholder="8.0" name="infrared4" onChange={this.handleChange} />
+                                <input type="text" class="form-control" placeholder="J" name="infrared5" onChange={this.handleChange} />
+                                <input type="text" class="form-control" placeholder="K" name="infrared6" onChange={this.handleChange} />
+                            
+                            </div>
+                                <div className="error">{this.state.infrared_e}</div>
+                            <div className="input-group">
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text" id="">Radio</span>
+                               
+                        
+                                <input type="text" class="form-control" placeholder="MHz" name="radio" onChange={this.handleChange} />
+
+                            </div>
+                                <div className="error">{this.state.radio_e}</div>
+                           
+                            
+</div>
+
+                    </div>
+
+
+{/* 
                     <InputGroup size="sm" className="mb-3" onChange={this.handleChange}>
                         <InputGroup.Prepend >
                             <InputGroup.Text id="inputGroup-sizing-sm" >Optical</InputGroup.Text>
                         </InputGroup.Prepend>
-                        <FormControl name="optical1" aria-label="Small" aria-describedby="inputGroup-sizing-sm" />  <div className="error">{this.state.optical1_e}</div>
+                        <FormControl name="optical1" aria-label="Small" aria-describedby="inputGroup-sizing-sm" />  
                     </InputGroup>
                    
 
@@ -296,7 +355,7 @@ class input_field extends React.Component {
                         </InputGroup.Prepend>
                         <FormControl name='radio' aria-label="Small" aria-describedby="inputGroup-sizing-sm" /> <div className="error">{this.state.radio_e}</div>
 
-                    </InputGroup>
+                    </InputGroup> */}
 
                     <Button type="submit">Submit</Button>
 
